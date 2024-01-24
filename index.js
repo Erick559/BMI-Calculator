77c8e5c7401f96b8783722700beefcc32f079e09
Erick <erickungu82@gmail.com>
2024-01-24 12:22:26 +0300

    Capture the gender,height and weight select elements and store them in their respective variables

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ const container = document.querySelector('.container')
 const height = document.querySelector('.height')
 const weight = document.querySelector('.weight')
 const age = document.querySelector('.age')
-
-console.table(height, weight, age)
+const gender = document.querySelector('.gender')
+const heightUnit = document.querySelector('.height-unit')
+const weightUnit = document.querySelector('.weight-unit')
