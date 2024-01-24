af756d02bf76b84e9182ee1f5ab1273570c158ed
Erick <erickungu82@gmail.com>
2024-01-24 12:27:04 +0300

    capture the submit button element and store it the calculateButton variable

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const container = document.querySelector('.container')
+
+// Variables needed for calculating the BMI
 const height = document.querySelector('.height')
 const weight = document.querySelector('.weight')
 const age = document.querySelector('.age')
 const gender = document.querySelector('.gender')
 const heightUnit = document.querySelector('.height-unit')
 const weightUnit = document.querySelector('.weight-unit')
+
+const calculateButton = document.querySelector('.submit')
