5ea6407f1ebdcf36439482f2dee0f497e4f76505
Erick <erickungu82@gmail.com>
2024-01-24 12:15:13 +0300

    Capture age, height, and weight input elements  assigning them to their respective variables for further processing

---
+++ index.js
@@ -0,0 +1,6 @@
+const container = document.querySelector('.container')
+const height = document.querySelector('.height')
+const weight = document.querySelector('.weight')
+const age = document.querySelector('.age')
+
+console.table(height, weight, age)
