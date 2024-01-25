d1dbbf83bb798d6535bc7715bb790ae698d52f3e
Erick559 <erickungu82@gmail.com>
2024-01-25 14:31:32 +0300

    Refactor the function getValue to getHeightValue
    The function takes the height unit selected and the height value and
    converts it to metres

--- index.js
+++ index.js
@@ -11,6 +11,20 @@ const weightUnit = document.querySelector('.weight-unit')
 const calculateButton = document.querySelector('.submit')
 
 // This function gets the values of the height and weight unit and returns the converted units which will be kg and meters
-function getValue(){
+function getHeightValue(){
+    let selectedHeightUnit = heightUnit.value
+    let heightInput;
 
+    if (selectedHeightUnit == 'inches'){
+        heightInput = height.value * 0.0254
+    }
+    else{
+        heightInput = height.value / 100;
+    }
+
+    return heightInput
 }
+
+calculateButton.addEventListener('click',() => {
+    console.log(getHeightValue())
+})
