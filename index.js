4632aeed78b4dd27c7b86b888ec63f0fa53f527c
Erick559 <erickungu82@gmail.com>
2024-01-28 13:46:48 +0300

    Add a function getRandomComments.
    This function returns a random comment that corresponds to the returned
    BMI weigh category

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const labels = document.querySelectorAll('label')
 const selectBoxes = document.querySelectorAll('select')
 const container = document.querySelector('.container')
 
-// Variables needed for appending the returned BMI results
+// Variables needed for appending the BMI results
 
 const header = document.querySelector('.header')
 const title = document.querySelector('.title')
@@ -24,13 +24,14 @@ const calculateButton = document.querySelector('button')
 
 // This function gets the value of the height unit and returns the converted unit which will be meters squared
 
-function getHeightValue() {
+function getHeightValue(){
     let selectedHeightUnit = heightUnit.value
     let heightInput;
 
-    if (selectedHeightUnit == 'inches') {
+    if (selectedHeightUnit == 'inches'){
         heightInput = parseFloat(height.value * 0.0254).toFixed(2)
-    } else {
+    }
+    else{
         heightInput = height.value / 100;
     }
 
@@ -39,13 +40,14 @@ function getHeightValue() {
 
 // This function gets the value of the weight unit and returns the converted unit which will be kg
 
-function getWeightValue() {
+function getWeightValue(){
     let selectedWeightUnit = weightUnit.value
     let weightInput;
 
-    if (selectedWeightUnit == 'pounds') {
+    if (selectedWeightUnit == 'pounds'){
         weightInput = (weight.value * 0.453592).toFixed(2)
-    } else {
+    }
+    else {
         weightInput = weight.value
     }
 
@@ -54,27 +56,30 @@ function getWeightValue() {
 
 // This function takes the heightValue and weightValue from their respective functions and returns the BMI
 
-function calculateBMI() {
+function calculateBMI(){
     let userHeight = getHeightValue()
     let userWeight = getWeightValue()
-    let BMI = userWeight / userHeight
+    let BMI = userWeight/userHeight
 
     return parseFloat(BMI).toFixed(2)
 }
 
 // This function returns the weight category according to the calculated BMI value
 
-function getWeightCategory() {
+function getWeightCategory(){
     let calculatedBMI = calculateBMI()
     let weightCategory;
 
-    if (calculatedBMI < 18.5) {
+    if (calculatedBMI <18.5){
         weightCategory = 'Underweight'
-    } else if (calculatedBMI >= 18.5 && calculatedBMI <= 24.9) {
+    }
+    else if (calculatedBMI >= 18.5 && calculatedBMI<= 24.9){
         weightCategory = 'Normal Weight'
-    } else if (calculatedBMI >= 25 && calculatedBMI <= 29.9) {
+    }
+    else if (calculatedBMI >= 25 && calculatedBMI <= 29.9){
         weightCategory = 'Overweight'
-    } else if (calculatedBMI >= 30) {
+    } 
+    else if (calculatedBMI >= 30 ){
         weightCategory = 'Obese'
     }
 
@@ -83,39 +88,40 @@ function getWeightCategory() {
 
 // This function changes the container's color according to the returned BMI weight category
 
-function changeContainerColor() {
+function changeContainerColor(){
     let returnedWeightCategory = getWeightCategory()
 
-    if (returnedWeightCategory === undefined) {
+    if(returnedWeightCategory === undefined){
         container.style.backgroundColor = '#F5F5F5'
-        inputBoxes.forEach(inputBox => inputBox.style.backgroundColor = '#F5F5F5')
-        selectBoxes.forEach(selectBox => selectBox.style.backgroundColor = '#F5F5F5')
-    } else {
-        switch (returnedWeightCategory) {
+        inputBoxes.forEach(inputBox => inputBox.style.backgroundColor ='#F5F5F5')
+        selectBoxes.forEach(selectBox => selectBox.style.backgroundColor='#F5F5F5')
+    }
+    else{
+        switch (returnedWeightCategory){
             case 'Underweight':
-                container.style.backgroundColor = '#87B1E3'
-                inputBoxes.forEach(inputBox => inputBox.style.backgroundColor = '#87B1E3')
-                selectBoxes.forEach(selectBox => selectBox.style.backgroundColor = '#87B1E3')
+                container.style.backgroundColor= '#87B1E3'
+                inputBoxes.forEach(inputBox => inputBox.style.backgroundColor ='#87B1E3')
+                selectBoxes.forEach(selectBox => selectBox.style.backgroundColor='#87B1E3')
                 break;
-
+    
             case 'Normal Weight':
                 container.style.backgroundColor = '#C1E898'
-                inputBoxes.forEach(inputBox => inputBox.style.backgroundColor = '#C1E898')
-                selectBoxes.forEach(selectBox => selectBox.style.backgroundColor = '#C1E898')
+                inputBoxes.forEach(inputBox => inputBox.style.backgroundColor ='#C1E898')
+                selectBoxes.forEach(selectBox => selectBox.style.backgroundColor='#C1E898')
                 break;
-
+            
             case 'Overweight':
                 container.style.backgroundColor = '#F9E488'
-                inputBoxes.forEach(inputBox => inputBox.style.backgroundColor = '#F9E488')
-                selectBoxes.forEach(selectBox => selectBox.style.backgroundColor = '#F9E488')
+                inputBoxes.forEach(inputBox => inputBox.style.backgroundColor ='#F9E488')
+                selectBoxes.forEach(selectBox => selectBox.style.backgroundColor='#F9E488')
                 break;
-
+            
             case 'Obese':
                 container.style.backgroundColor = '#F38B8B'
-                inputBoxes.forEach(inputBox => inputBox.style.backgroundColor = '#F38B8B')
-                selectBoxes.forEach(selectBox => selectBox.style.backgroundColor = '#F38B8B')
+                inputBoxes.forEach(inputBox => inputBox.style.backgroundColor ='#F38B8B')
+                selectBoxes.forEach(selectBox => selectBox.style.backgroundColor='#F38B8B')
         }
-
+    
         labels.forEach(label => label.style.color = 'white')
         calculateButton.classList.add('calculate')
         container.appendChild(calculateButton)
@@ -124,27 +130,40 @@ function changeContainerColor() {
 
 // This function appends the calculated BMI results to the header container
 
-function appendResults() {
+function appendResults(){
     let bmiResult = calculateBMI()
-
-    if (bmiResult === 'NaN') {
+    let comments = getRandomComment(getWeightCategory())
+  
+    if(bmiResult === 'NaN'){
         mainSection.textContent = 'FILL IN DETAILS CORRECTLY'
-        mainSection.style.color = 'red'
-    } else {
+        mainSection.style.color= 'red'
+    }
+    else{
         title.textContent = 'YOUR RESULT'
         mainSection.textContent = bmiResult
 
         mainSection.style.fontSize = '50px'
         mainSection.style.color = 'white'
-        
+
         title.style.color = 'white'
         title.style.fontWeight = '700'
     }
 }
 
+function getRandomComment(weightCategory) {
+    const comments = {
+      "Underweight": ["You may want to consider gaining some weight.", "Eat a balanced diet for better health."],
+      "Normal Weight": ["You're in a healthy weight range. Keep it up!", "Maintaining a healthy weight is important for overall well-being."],
+      "Overweight": ["Consider making lifestyle changes to achieve a healthier weight.", "Regular exercise and a balanced diet can help with weight management."],
+      "Obese": ["It's important to address obesity for long-term health.", "Consult with a healthcare professional for personalized advice."],
+    };
+  
+    return comments[weightCategory] ? comments[weightCategory][Math.floor(Math.random() * comments[weightCategory].length)] : "";
+  }
 
-calculateButton.addEventListener('click', () => {
-    console.log(calculateBMI())
+
+calculateButton.addEventListener('click',() => {
+    console.log(getRandomComment(getWeightCategory()));
     changeContainerColor()
     appendResults()
 })
