da2eef80c687c1415b37c10b8060f5ab811fac89
Erick559 <erickungu82@gmail.com>
2024-02-14 13:09:24 +0300

    Fix variable values.

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // Variables needed for calculating the BMI
 
-const height = document.querySelector('.height')
-const weight = document.querySelector('.weight')
+const height = document.querySelector('#height')
+const weight = document.querySelector('#weight')
 const age = document.querySelector('.age')
 const gender = document.querySelector('.gender')
-const heightUnit = document.querySelector('.height-unit')
-const weightUnit = document.querySelector('.weight-unit')
+const heightUnit = document.querySelector('#height-unit')
+const weightUnit = document.querySelector('#weight-unit')
 
 //Variables needed for the background color change
 
